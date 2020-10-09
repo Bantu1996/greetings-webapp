39d3c5a2b3bb3fc671aba1efb23f9287d945df5c
Bantu1996 <sotyatobantu@gmail.com>
2020-10-09 16:10:41 +0200

    added more changes

--- test/greet.test.js
+++ test/greet.test.js
@@ -19,11 +19,22 @@ describe('Greetings', async function(){
         await pool.query("delete from greeting;");
         
     });
-    describe("enter the name of a user", async function() {
-    it("should be able to greet a name", async function () {
+    describe("Names entered", async function() {
+    it("should be able to enter a name once greet it more than once", async function () {
 
 		await pool.query(INSERT_QUERY, ["Snowy", 4]);
-		await pool.query(INSERT_QUERY, ["Spotty", 7]);
+	
+
+		const results = await pool.query("select count(*) from greeting");
+		
+		// how many greeting should have been added?
+		assert.equal(1, results.rows[0].count);
+
+    });
+    it("should be able to count  greeted  names", async function () {
+
+		await pool.query(INSERT_QUERY, ["Snowy", 4]);
+        await pool.query(INSERT_QUERY, ["Spotty", 3]);
 
 		const results = await pool.query("select count(*) from greeting");
 		
@@ -47,10 +58,77 @@ describe('Greetings', async function(){
     })
 
     describe("radio buttons", async function() {
-        it ("should be able to greet any name in IsiXhosa.", async function () {
-            
+
+            it ("should be able to greet any name in IsiXhosa.", async function () {
+
+                let greetThem = GreetWithRespect(pool);  
+               
+      
+                await greetThem.greetingLanguages("IsiXhosa", "Bantu");
+              const respe = await greetThem.greetingLanguages("IsiXhosa", "Bantu")
+                assert.equal("Molo, Bantu", respe);
+          
+              });
+              it ("should be able to greet any name in English.", async function () {
+      
+                let greetThem = GreetWithRespect(pool);  
+               
+      
+                await greetThem.greetingLanguages("English", "Bantu");
+              const respe = await greetThem.greetingLanguages("English", "Bantu")
+                assert.equal("Hello, Bantu", respe);
+          
+              });
+              it ("should be able to greet any name in Afrikaans.", async function () {
+      
+                let greetThem = GreetWithRespect(pool);  
+               
+      
+                await greetThem.greetingLanguages("Afrikaans", "Bantu");
+              const respe = await greetThem.greetingLanguages("Afrikaans", "Bantu")
+                assert.equal("Goeie More, Bantu", respe);
+          
+              }); 
         })
-    })
+    
+
+        describe("Counter", async function() {
+            it("should be able to count a greeted  name", async function () {
+        
+                await pool.query(INSERT_QUERY, ["Snowy", 4]);
+            
+        
+                const results = await pool.query("select count(*) from greeting");
+                
+                // how many greeting should have been added?
+                assert.equal(1, results.rows[0].count);
+        
+            });
+            it("should be able to count  greeted  names", async function () {
+        
+                await pool.query(INSERT_QUERY, ["Snowy", 4]);
+                await pool.query(INSERT_QUERY, ["Spotty", 3]);
+        
+                const results = await pool.query("select count(*) from greeting");
+                
+                // how many greeting should have been added?
+                assert.equal(2, results.rows[0].count);
+        
+            });
+            
+            it("should be able to find all greetings made", async function () {
+        
+                await pool.query(INSERT_QUERY, ["Snowy", 4,]);
+                await pool.query(INSERT_QUERY, ["Spotty", 3]);
+                await pool.query(INSERT_QUERY, ["Kitty", 7]);
+        
+                const results = await pool.query("select count(*) from greeting");
+        
+                // how many greeting should be found?
+                assert.equal(3, results.rows[0].count);
+                
+            });
+        })
     
     after(function(){
         pool.end();
